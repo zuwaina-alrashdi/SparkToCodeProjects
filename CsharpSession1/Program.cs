1f00c3bcc75f60c32499be5c2cb4bb4e56146c6a
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-06-30 19:50:49 +0400

    Added basic calculator operations

--- CsharpSession1/Program.cs
+++ CsharpSession1/Program.cs
@@ -20,8 +20,27 @@
             //Console.WriteLine("hello " + userName + "welcome to spark");
 
             ////////////////////////////////////////////////////////////////////////////
-            
 
+            Console.WriteLine("enter first number: ");
+            float firstNumber = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter second number: ");
+            float secondNumber = float.Parse(Console.ReadLine());
+
+            float additionResult = firstNumber + secondNumber;
+            float subtractionResult = firstNumber - secondNumber;
+            float multiplicationResult = firstNumber * secondNumber;
+            float divisionResult = firstNumber / secondNumber;
+            float reminderResult = firstNumber % secondNumber;
+            bool comparisonResult = firstNumber == secondNumber;
+            bool testResult = !(10 > 5); // false
+
+            Console.WriteLine("Addition result: " + additionResult);
+            Console.WriteLine("Subtraction result: " + subtractionResult);
+            Console.WriteLine("multiplication result: " + multiplicationResult);
+            Console.WriteLine("Division result: " + divisionResult);
+            Console.WriteLine("Reminder result: " + reminderResult);
+            Console.WriteLine("Comparison result: " + comparisonResult);
         }
     }
 }
