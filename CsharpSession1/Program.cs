06afc7a2ef9eda1cc9eb985dfde2650cd3b2ae25
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-06-30 19:55:13 +0400

    Implement grade evaluation using if-else

--- CsharpSession1/Program.cs
+++ CsharpSession1/Program.cs
@@ -21,26 +21,63 @@
 
             ////////////////////////////////////////////////////////////////////////////
 
-            Console.WriteLine("enter first number: ");
-            float firstNumber = float.Parse(Console.ReadLine());
+            //Console.WriteLine("enter first number: ");
+            //float firstNumber = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter second number: ");
-            float secondNumber = float.Parse(Console.ReadLine());
+            //Console.WriteLine("enter second number: ");
+            //float secondNumber = float.Parse(Console.ReadLine());
 
-            float additionResult = firstNumber + secondNumber;
-            float subtractionResult = firstNumber - secondNumber;
-            float multiplicationResult = firstNumber * secondNumber;
-            float divisionResult = firstNumber / secondNumber;
-            float reminderResult = firstNumber % secondNumber;
-            bool comparisonResult = firstNumber == secondNumber;
-            bool testResult = !(10 > 5); // false
+            //float additionResult = firstNumber + secondNumber;
+            //float subtractionResult = firstNumber - secondNumber;
+            //float multiplicationResult = firstNumber * secondNumber;
+            //float divisionResult = firstNumber / secondNumber;
+            //float reminderResult = firstNumber % secondNumber;
+            //bool comparisonResult = firstNumber == secondNumber;
+            //bool testResult = !(10 > 5); // false
 
-            Console.WriteLine("Addition result: " + additionResult);
-            Console.WriteLine("Subtraction result: " + subtractionResult);
-            Console.WriteLine("multiplication result: " + multiplicationResult);
-            Console.WriteLine("Division result: " + divisionResult);
-            Console.WriteLine("Reminder result: " + reminderResult);
-            Console.WriteLine("Comparison result: " + comparisonResult);
+            //Console.WriteLine("Addition result: " + additionResult);
+            //Console.WriteLine("Subtraction result: " + subtractionResult);
+            //Console.WriteLine("multiplication result: " + multiplicationResult);
+            //Console.WriteLine("Division result: " + divisionResult);
+            //Console.WriteLine("Reminder result: " + reminderResult);
+            //Console.WriteLine("Comparison result: " + comparisonResult);
+
+            ////////////////////////////////////////////////////////////////////////////
+
+            //// and:  &&
+            //// Or:   ||
+            //// not:  !
+
+
+            //Console.WriteLine("Enter your degree: ");
+            //float degree = float.Parse(Console.ReadLine());
+
+            //if (degree >= 0 && degree < 50)
+            //{
+            //    Console.WriteLine("you failed");
+            //}
+            //else if (degree >= 50 && degree < 60)
+            //{
+            //    Console.WriteLine("you passed with Grade: D");
+            //}
+            //else if (degree >= 60 && degree < 70)
+            //{
+            //    Console.WriteLine("you passed with Grade: C");
+            //}
+            //else if (degree >= 70 && degree < 80)
+            //{
+            //    Console.WriteLine("you passed with Grade: B");
+            //}
+            //else if (degree > 80)
+            //{
+
+            //    Console.WriteLine("you passed with Grade: A");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you entered a wrong number");
+            //}
+
         }
     }
 }
