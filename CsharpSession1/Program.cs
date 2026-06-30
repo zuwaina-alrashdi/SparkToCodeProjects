95f2d1c788bcaaec7b86e66b6dc74cb46eb7aae7
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-06-30 19:41:27 +0400

    Add project files.

---
+++ CsharpSession1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpSession1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to show welcome message
+
+
+            //Console.Write("Please Enter Your Name: ");
+            //string userName = Console.ReadLine();
+
+            //Console.Write("Please Enter Your Age: ");
+            //int userAge = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("please enter your salary: ");
+            //float userSalary = float.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("hello " + userName + " welcome to spark");
+
+            ////////////////////////////////////////////////////////////////////////////
+            
+
+        }
+    }
+}
