0fcd765c2003ba281ba1e3a6018dbbbf7a1c7788
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-01 08:33:32 +0400

    Added personal info card

---
+++ CsharpTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CsharpTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1 - Personal Info Card
+
+            string name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Student: " + isStudent);
+        }
+    }
+}
