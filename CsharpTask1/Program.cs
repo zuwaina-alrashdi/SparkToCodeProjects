5f4640a8cebb5caa586790440cdecdc953d3663b
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-01 08:51:36 +0400

    Added Rectangle Calculator

--- CsharpTask1/Program.cs
+++ CsharpTask1/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace CsharpTask1
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace CsharpTask1
 {
     internal class Program
     {
@@ -6,15 +9,32 @@
         {
             // Task 1 - Personal Info Card
 
-            string name = "Sara";
-            int age = 21;
-            double height = 1.65;
-            bool isStudent = true;
+            //string name = "Sara";
+            //int age = 21;
+            //double height = 1.65;
+            //bool isStudent = true;
 
-            Console.WriteLine("Name: " + name);
-            Console.WriteLine("Age: " + age);
-            Console.WriteLine("Height: " + height);
-            Console.WriteLine("Student: " + isStudent);
+            //Console.WriteLine("Name: " + name);
+            //Console.WriteLine("Age: " + age);
+            //Console.WriteLine("Height: " + height);
+            //Console.WriteLine("Student: " + isStudent);
+
+            ////////////////////////////////////////////////////
+
+            // Task 2 - Rectangle Calculator
+
+            Console.Write("Please Enter the Rectangle Length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter the Rectangle Width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = length * width;
+            double perimeter = 2 * (length + width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("perimeter: " + perimeter);
+
         }
     }
 }
