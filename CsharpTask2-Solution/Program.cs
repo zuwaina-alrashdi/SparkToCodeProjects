343f3a97c6ad4aacaf8d5824ac675760daacd7ca
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-02 08:47:52 +0400

    Add project files

---
+++ CsharpTask2-Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpTask2_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
