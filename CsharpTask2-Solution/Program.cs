1f0ce30c9b14c07ee7e650e26244a27c6f81a639
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-02 09:55:45 +0400

    Implemented problem 2 (Sum of Numbers 1 to N)

--- CsharpTask2-Solution/Program.cs
+++ CsharpTask2-Solution/Program.cs
@@ -6,15 +6,32 @@
         {
             // Task 1 - Countdown Timer
 
-            Console.Write("Enter a starting number: ");
-            int startNumber = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a starting number: ");
+            //int startNumber = int.Parse(Console.ReadLine());
 
-            for (int counter = startNumber; counter >= 1; counter--)
+            //for (int counter = startNumber; counter >= 1; counter--)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            //Console.WriteLine("Liftoff!");
+
+            //////////////////////////////////////////////////////////
+
+            // Task 2 - Sum of Numbers 1 to N
+
+            Console.Write("Enter a positive whole number: ");
+            int positiveNumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int counter = 1; counter <= positiveNumber; counter++)
             {
-                Console.WriteLine(counter);
+                sum = sum + counter;
             }
 
-            Console.WriteLine("Liftoff!");
+            Console.WriteLine("The sum is: " + sum);
+
         }
     }
 }
