77c9700531c5dc1d82a96c8cb13394dd4ba6f573
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-02 07:36:20 +0400

    Added basic for loop example

---
+++ CsharpSession2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpSession2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //looping 
+            //1- for loop => countable loop
+            //2- while loop => uncountable loop
+
+            //For loop:
+            //  for( start; end; increment )
+            //  {  
+            //         some code to be repeated    
+            //   }
+
+
+            int counter = 0;
+
+            for (counter = 10; counter >= 5; counter--)
+            {
+                Console.WriteLine("certain message loop number:" + counter);
+
+            }
+        }
+    }
+}
