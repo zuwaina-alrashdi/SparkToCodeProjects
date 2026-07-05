0579fd8dba8ae347c0bf1eb947acf86c656e1f8b
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-05 17:43:46 +0400

    Implemented problem 1 (Absolute Difference)

--- CsharpFunctions/Program.cs
+++ CsharpFunctions/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Absolute Difference
+
+            Console.Write("Enter the first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double difference = firstNumber - secondNumber;
+            double absoluteDifference = Math.Abs(difference);
+
+            Console.WriteLine("Positive difference: " + absoluteDifference);
         }
     }
 }
