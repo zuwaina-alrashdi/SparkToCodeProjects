dfb65d7bcc868a857f8caf0ccea7d6da7c463cfd
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-05 17:50:34 +0400

    Implemented problem 3 (Name Formatter)

--- CsharpFunctions/Program.cs
+++ CsharpFunctions/Program.cs
@@ -21,14 +21,29 @@
 
             // Task 2 - Power & Root Explorer
 
-            Console.Write("Enter a number: ");
-            double number = double.Parse(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //double number = double.Parse(Console.ReadLine());
 
-            double square = Math.Pow(number, 2);
-            double squareRoot = Math.Sqrt(number);
+            //double square = Math.Pow(number, 2);
+            //double squareRoot = Math.Sqrt(number);
 
-            Console.WriteLine("Square: " + square);
-            Console.WriteLine("Square Root: " + squareRoot);
+            //Console.WriteLine("Square: " + square);
+            //Console.WriteLine("Square Root: " + squareRoot);
+
+            ///////////////////////////////////////////////////////////////////
+
+            // Task 3 - Name Formatter
+
+            Console.Write("Enter your full name: ");
+            string fullName = Console.ReadLine();
+
+            string upperName = fullName.ToUpper();
+            string lowerName = fullName.ToLower();
+            int nameLength = fullName.Length;
+
+            Console.WriteLine("Uppercase: " + upperName);
+            Console.WriteLine("Lowercase: " + lowerName);
+            Console.WriteLine("Number of characters: " + nameLength);
         }
     }
 }
