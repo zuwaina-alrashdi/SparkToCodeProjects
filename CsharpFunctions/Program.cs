ca581bfdba66f52b021119082a2ea4496c771fce
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-05 17:48:24 +0400

    Implemented problem 2 (Power & Root Explorer)

--- CsharpFunctions/Program.cs
+++ CsharpFunctions/Program.cs
@@ -6,16 +6,29 @@
         {
             // Task 1 - Absolute Difference
 
-            Console.Write("Enter the first number: ");
-            double firstNumber = double.Parse(Console.ReadLine());
+            //Console.Write("Enter the first number: ");
+            //double firstNumber = double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the second number: ");
-            double secondNumber = double.Parse(Console.ReadLine());
+            //Console.Write("Enter the second number: ");
+            //double secondNumber = double.Parse(Console.ReadLine());
 
-            double difference = firstNumber - secondNumber;
-            double absoluteDifference = Math.Abs(difference);
+            //double difference = firstNumber - secondNumber;
+            //double absoluteDifference = Math.Abs(difference);
 
-            Console.WriteLine("Positive difference: " + absoluteDifference);
+            //Console.WriteLine("Positive difference: " + absoluteDifference);
+
+            ///////////////////////////////////////////////////////////////////
+
+            // Task 2 - Power & Root Explorer
+
+            Console.Write("Enter a number: ");
+            double number = double.Parse(Console.ReadLine());
+
+            double square = Math.Pow(number, 2);
+            double squareRoot = Math.Sqrt(number);
+
+            Console.WriteLine("Square: " + square);
+            Console.WriteLine("Square Root: " + squareRoot);
         }
     }
 }
