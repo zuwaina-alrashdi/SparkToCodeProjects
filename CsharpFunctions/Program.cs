74475945f347f680cdd228ea3ecaa087a035fc3b
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-05 17:15:52 +0400

    Renamed the projects

---
+++ CsharpFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsharpFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
