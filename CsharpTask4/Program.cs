8a68580eeb08acedf525fb9726e3df8afcff2914
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-06 17:51:14 +0400

    Implemeted problem 2 (Square Number Function)

--- CsharpTask4/Program.cs
+++ CsharpTask4/Program.cs
@@ -6,14 +6,31 @@
         {
             // Task 1 - Personalized Welcome Function
 
-            Console.Write("Enter your name: ");
-            string userName = Console.ReadLine();
+            //    Console.Write("Enter your name: ");
+            //    string userName = Console.ReadLine();
 
-            PrintWelcome(userName);
+            //    PrintWelcome(userName);
+            //}
+            //static void PrintWelcome(string name)
+            //{
+            //    Console.WriteLine("Welcome, " + name + "!");
+
+            /////////////////////////////////////////////////////
+
+            // Task 2 - Square Number Function
+
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Square(number);
+
+            Console.WriteLine("Square = " + result);
         }
-        static void PrintWelcome(string name)
+
+        static int Square(int number)
         {
-            Console.WriteLine("Welcome, " + name + "!");
+            return number * number;
+
         }
     }
 }
