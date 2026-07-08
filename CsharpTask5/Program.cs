137d7198bd5a09b8d817fb5904fc02a2f0755f66
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-08 18:27:53 +0400

    implemented problem 1 (Fixed Grades Array)

--- CsharpTask5/Program.cs
+++ CsharpTask5/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1 - Fixed Grades Array
+
+            int[] grades = new int[5];
+
+            for (int counter = 0; counter < grades.Length; counter++)
+            {
+                Console.Write("Enter grade " + (counter + 1) + ": ");
+                grades[counter] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Student Grades:");
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
         }
     }
 }
