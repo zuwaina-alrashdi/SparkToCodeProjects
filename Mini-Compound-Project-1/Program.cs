309e86de599f419123d8cb433e87071b10b08a7c
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-09 20:06:22 +0400

    Add project files for Mini Compound Project 1

---
+++ Mini-Compound-Project-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_Compound_Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
