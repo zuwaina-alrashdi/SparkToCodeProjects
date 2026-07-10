45945e82c77bb23c96780cfe1a66cbd82565d08d
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-10 11:10:52 +0400

    Implemented Service 1 (Add New Account)

--- Mini-Compound-Project-1/Program.cs
+++ Mini-Compound-Project-1/Program.cs
@@ -2,9 +2,48 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+            static List<string> customerNames = new List<string>();
+            static List<string> accountNumbers = new List<string>();
+            static List<double> balances = new List<double>();
+
+            static void Main(string[] args)
+            {
+              AddAccount();
+            }
+
+        // Service 1 - Add New Account
+        static void AddAccount()
+            {
+                Console.Write("Enter customer name: ");
+                string customerName = Console.ReadLine();
+
+                Console.Write("Enter account number: ");
+                string accountNumber = Console.ReadLine();
+
+                if (accountNumbers.Contains(accountNumber))
+                {
+                    Console.WriteLine("This account number already exists.");
+                    return;
+                }
+
+                Console.Write("Enter initial deposit: ");
+                double balance = double.Parse(Console.ReadLine());
+
+                if (balance < 0)
+                {
+                    Console.WriteLine("Initial deposit cannot be negative.");
+                    return;
+                }
+
+                customerNames.Add(customerName);
+                accountNumbers.Add(accountNumber);
+                balances.Add(balance);
+
+                Console.WriteLine("Account created successfully!");
+                Console.WriteLine("Customer Name: " + customerName);
+                Console.WriteLine("Account Number: " + accountNumber);
+                Console.WriteLine("Balance: " + balance);
+
+            }
     }
 }
