6caf2c1c3c638426e65b187108a74382d28ddf2c
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-07-15 16:29:14 +0400

    Add Room Class

---
+++ OOP_Part2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace OOP_Part2
+{
+    internal class Program
+    {
+        public class Room
+        {
+            public int roomNumber;
+            public string roomType;
+            public double pricePerNight;
+            public bool isAvailable;
+
+            public Room(int roomNumber, string roomType, double pricePerNight, bool isAvailable)
+            {
+                this.roomNumber = roomNumber;
+                this.roomType = roomType;
+                this.pricePerNight = pricePerNight;
+                this.isAvailable = isAvailable;
+            }
+
+            public void displayRoom()
+            {
+                Console.WriteLine("Room Number: " + roomNumber);
+                Console.WriteLine("Room Type: " + roomType);
+                Console.WriteLine("Price Per Night: " + pricePerNight);
+                Console.WriteLine("Available: " + isAvailable);
+            }
+        }
+
+    }
+}
+
